d893a6c576f7338d0fb1b127a5690fd01a16a8b8
artuncingoz <artuncingoz2@gmail.com>
2025-11-11 17:20:52 +0300

    Added tork diff span, fixing css weight ball placements and added transform into plank variable.  Added updateSeesaw function into script to calculate tork and weight diff in plank so the plank may transform its position by that. Also added next weight for better end-user experience

--- script.js
+++ script.js
@@ -2,11 +2,46 @@
 
 const plank = document.getElementById('plank');
 const leftWeightDisplay = document.getElementById('left_weight');
-const rightWeightDisplay = document.getElementById('right-weight');
+const rightWeightDisplay = document.getElementById('right_weight');
 const resetButton = document.getElementById('reset-button');
 const next_weight = document.getElementById('next_weight');
+const torqdiff_display = document.getElementById('tork_diff');
 
 let objects = [];
+let nextWeightValues = [Math.floor(Math.random() * 10) + 1, Math.floor(Math.random() * 10) + 1];
+
+function updateSeesaw() {
+    let leftWeight = 0;
+    let rightWeight = 0;
+    let leftTorque = 0;
+    let rightTorque = 0;
+    const plankCenter = plank.clientWidth / 2;
+
+    objects.forEach(obj => {
+        if (obj.position < plankCenter) {
+            leftWeight += obj.weight;
+            leftTorque += obj.weight * (plankCenter - obj.position);
+        } else {
+            rightWeight += obj.weight;
+            rightTorque += obj.weight * (obj.position - plankCenter);
+        }
+    });
+
+    leftWeightDisplay.textContent = leftWeight;
+    rightWeightDisplay.textContent = rightWeight;
+
+    const torqdiff = -(leftTorque - rightTorque);
+
+    torqdiff_display.textContent = torqdiff;
+
+    if (Math.abs(torqdiff * 0.005) > 20) {
+        plank.style.transform = `translateX(-50%) rotate(${torqdiff > 0 ? 20 : -20}deg)`;
+        return;
+    }
+
+    plank.style.transform = `translateX(-50%) rotate(${torqdiff * 0.005}deg)`;
+}
+
 
 function createWeightElement(weightObject) {
     const weightElement = document.createElement('div');
@@ -24,8 +59,10 @@ function addWeight(event) {
 
     const clickPosition = event.offsetX;
 
-    const newWeight = Math.floor(Math.random() * 10) + 1;
-
+    // Get the next weight from the queue and delete it from the list 
+    const newWeight = nextWeightValues.shift();
+    nextWeightValues.push(Math.floor(Math.random() * 10) + 1);
+    next_weight.textContent = nextWeightValues[0];
 
     let newWeightObject = {
         weight: newWeight,
@@ -36,16 +73,19 @@ function addWeight(event) {
 
     createWeightElement(newWeightObject);
 
+    updateSeesaw();
 }
 
 function resetWeights() {
     objects = [];
     plank.innerHTML = '';
+    updateSeesaw();
 }
 
 function init() {
     resetButton.addEventListener('click', resetWeights);
     plank.addEventListener('click', addWeight);
+    next_weight.textContent = nextWeightValues[0];
 }
 
 
